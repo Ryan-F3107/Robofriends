7027b7b8033bca6e1c20c799234ea999295b280b
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2019-12-18 23:35:01 -0500

    Done with Creating an App with React #3

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@ import React from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
-
+import './App.css';
 class App extends React.Component {
 	constructor() {
 		super()
-		this.state = {
+		this.state = {	//two states, any component that has state while use this.state, it describes the App
 			robots: robots,
 			searchfield: ''
-		}
+		}	//react renders and uses these states as props
 	}
 
 	onSearchChange = (event) => {
 		this.setState({ searchfield: event.target.value})	//anytime we want to change state in React
 		
 	}
+
 	render() {
-		const filteredRobots = this.state.robots.filter(robot =>{
+		const filteredRobots = this.state.robots.filter(robot =>{ // A new array is basically created
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) //if the robot includes anything in the seach box
 		})
 		return (
 			<div className='tc'>
-				<h1>RoboFriends</h1>
+				<h1 className = 'f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
 				<CardList robots={filteredRobots}/>
 			</div>
