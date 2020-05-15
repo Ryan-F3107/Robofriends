b8ce238a6810f48bc79e508fccd5a42fb31577ab
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2020-05-15 00:04:03 -0400

    added the reducer

---
+++ src/action.js
@@ -0,0 +1,7 @@
+import {CHANGE_SEARCH_FIELD} from './constant.js'	//good practise to find error
+
+export const setSearchField = (text) => ({
+	type: CHANGE_SEARCH_FIELD,	//usually CAP is const , standard
+	payload: text
+})	//it will take text and return an object and send a text to user as payload
+// () used, it significants returning something.
---
+++ src/reducers.js
@@ -0,0 +1,17 @@
+import { CHANGE_SEARCH_FIELD } from './constant.js';
+const initialState = {
+	searchField: ''
+}
+//Now we create a reducer that outputs a state
+//actions are just objects that we are returning 
+// we give the two parameters default values using ES6 
+// REDUCER
+export const searchRobots = (state=initialState, action={})	=> {
+	//we act upon the state based on action
+	switch(action.type) {
+		case: CHANGE_SEARCH_FIELD:
+			return Object.assign({}, state, {searchField:action.payload});	//we return a new state
+		default:
+			return state;	//we could have used an if statement
+	}	//end of switch statement.	
+}
